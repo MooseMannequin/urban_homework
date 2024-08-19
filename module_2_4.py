7a89ad8c69094b74984afb196f65e64953e8073d
MooseMannequin <EugeneNeudene@yandex.ru>
2024-08-19 11:59:05 +0300

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,7 +1,6 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-is_prime = True
 for i in numbers[1:15]:  # i = 2
     # print(numbers[i])
     for j in numbers[1:15]:  # j = 2,3,4
