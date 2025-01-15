883a1555d3aa7004bb9018e5c7f5d77fface2035
MooseMannequin <EugeneNeudene@yandex.ru>
2025-01-15 21:39:25 +0300

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -13,7 +13,7 @@ async def admin_login() -> str:
     return "Вы вошли как администратор"
 
 @app.get('/user/{user_id}')
-async def user_login(user_id: int = Path(ge=1, le=100, description='Enter User ID', example=7)) -> str:
+async def user_login(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example=7)]) -> str:
     return f"Вы вошли как пользователь № {user_id}"
 
 @app.get('/user/{username}/{age}')
